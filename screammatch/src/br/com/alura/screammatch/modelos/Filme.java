126a10123d1c312b9f85afe7c23342f7e21dbd1e
Fabiana Mendes <feelpitymendes@gmail.com>
2024-04-03 12:31:21 -0300

    CHANGES

---
+++ screammatch/src/br/com/alura/screammatch/modelos/Filme.java
@@ -0,0 +1,14 @@
+package br.com.alura.screammatch.modelos;
+
+public class Filme extends Titulo{
+    //Atb da Class
+    private  String diretor;
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+}
